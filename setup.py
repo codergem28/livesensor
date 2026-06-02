8e9ecfa933042bc84ad9c5e04222ea8e00c09c26
sudhanshu tripathi <sudhanshumtripathi28@gmail.com>
2026-06-02 19:52:24 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages ,setup
+from typing import List
+
+
+def get_requirements()->list[str]:
+
+    requirements_list = list[str] = []
+
+    return requirements_list
+
+setup(
+
+    name= 'sensor',
+    version="0.0.1",
+    author="sudhanshu",
+    author_email="sudhanshumtripathi28@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)

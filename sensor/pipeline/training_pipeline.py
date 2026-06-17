e729df4c778de8039de0358f9db6640985f5d4d7
sudhanshu tripathi <sudhanshumtripathi28@gmail.com>
2026-06-17 19:10:46 +0530

    updated till transformation

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -13,10 +13,10 @@ from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConf
 from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 
 
-#from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig
-#from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact,DataTransformationArtifact
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact,DataTransformationArtifact
 
-#from sensor.components.data_transformation import DataTransformation
+from sensor.components.data_transformation import DataTransformation
 
 
 class TrainingPipeline:
@@ -58,7 +58,23 @@ class TrainingPipeline:
         except  Exception as e:
             raise  SensorException(e,sys)
         
+    def start_data_transformation(self,
+                              data_validation_artifact: DataValidationArtifact):
+        try:
+            data_transformation_config = DataTransformationConfig(
+            training_pipeline_config=self.training_pipeline_config
+        )
 
+            data_transformation = DataTransformation(
+            data_validation_artifact=data_validation_artifact,
+            data_transformation_config=data_transformation_config
+        )
+
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+
+        except Exception as e:
+          raise SensorException(e, sys)
         
     def run_pipeline(self):
         try:
@@ -71,7 +87,7 @@ class TrainingPipeline:
 
 
 
-            #data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+            data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
 
             
         except Exception as e :    
--- sensor/utils/main__utils.py
+++ sensor/utils/main__utils.py
@@ -5,6 +5,7 @@ import os
 import dill
 import sys
 from sensor.exception import SensorException
+import logging
 
 def read_yaml_file(file_path:str)->dict:
     try:
@@ -23,9 +24,52 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
             if os.path.exists(file_path):
                 os.remove(file_path)
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        with open(file_path, "w") as file:
-            yaml.dump(content, file)
+        with open(file_path, "w") as file_obj:
+            yaml.dump(content, file_obj)
 
     except Exception as e:
         raise SensorException(e, sys)          
+
+
+def save_numpy_array_data(file_path:str,array:np.array):
+    """
+    Save numpy array to data file file_path:str
+      location of file to array:np.array 
+      data to save   
+    """
+
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            np.save(file_obj,array)
+
+    except Exception as e:
+      raise SensorException(e.sys) from e
+    
+
+def load_numpy_array_data(file_path:str) -> np.array:
+    """
+    load numpy array data from file
+    file_path:str location of file to load
+    return:np.array data loaded
+    
+    """
+    try:
+        with open(file_path,"rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise SensorException(e,sys) from e
+
+
+def save_object(file_path:str,obj:object) -> None:
+    try:
+        logging.info("Entered the save_object method of mainUtils class")
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+            logging.info("Exited the save_object method in Main_utils class")
+
+    except Exception as e:
+        raise SensorException(e,sys) from e
 
